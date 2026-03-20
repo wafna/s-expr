ed130e9078f3a99cf24170ed3046ae6091678ecb
Jason Feingold <jfeingold@gmail.com>
2026-03-19 22:10:17 -0400

    misc

--- lib/src/test/kotlin/wafna/sexpr/TestWriter.kt
+++ lib/src/test/kotlin/wafna/sexpr/TestWriter.kt
@@ -1,29 +1,7 @@
 package wafna.sexpr
 
 import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.fail
 import java.io.ByteArrayOutputStream
-
-fun compareSExpr(actual: SExpr, expected: SExpr) {
-    when (actual) {
-        is SAtom -> when (expected) {
-            is SAtom -> assertEquals(expected.data.toString(Charsets.UTF_8), actual.data.toString(Charsets.UTF_8))
-            is SList -> fail("Incompatible: expected SList, got SAtom")
-        }
-
-        is SList -> when (expected) {
-            is SList -> {
-                assertEquals(expected.exprs.size, actual.exprs.size)
-                expected.exprs.zip(actual.exprs).forEach { (a, b) ->
-                    compareSExpr(a, b)
-                }
-            }
-
-            is SAtom -> fail("Incompatible: expected SAtom, got SList")
-        }
-    }
-}
 
 class TestWriter {
     fun testExpr(f: SExprBuilder.() -> Unit) {
--- lib/src/test/kotlin/wafna/sexpr/Util.kt
+++ lib/src/test/kotlin/wafna/sexpr/Util.kt
@@ -1,6 +1,7 @@
 package wafna.sexpr
 
 import kotlin.test.assertEquals
+import kotlin.test.fail
 
 fun String.toCharStream() = CharStream(iterator())
 
@@ -9,5 +10,25 @@ fun parse(input: String) = parse(input.toCharStream())
 fun assertAtom(expected: SExpr, actual: String) {
     val expected = expected as SAtom
     assertEquals(String(expected.data), actual)
+}
+
+fun compareSExpr(actual: SExpr, expected: SExpr) {
+    when (actual) {
+        is SAtom -> when (expected) {
+            is SAtom -> assertEquals(expected.data.toString(Charsets.UTF_8), actual.data.toString(Charsets.UTF_8))
+            is SList -> fail("Incompatible: expected SList, got SAtom")
+        }
+
+        is SList -> when (expected) {
+            is SList -> {
+                assertEquals(expected.exprs.size, actual.exprs.size)
+                expected.exprs.zip(actual.exprs).forEach { (a, b) ->
+                    compareSExpr(a, b)
+                }
+            }
+
+            is SAtom -> fail("Incompatible: expected SAtom, got SList")
+        }
+    }
 }
 
