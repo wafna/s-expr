d0729e025079ae1913e9c92f380dc4974273946c
Jason Feingold <jfeingold@gmail.com>
2026-03-19 22:31:57 -0400

    tidy

--- lib/src/main/kotlin/wafna/sexpr/Parser.kt
+++ lib/src/main/kotlin/wafna/sexpr/Parser.kt
@@ -1,18 +1,18 @@
 package wafna.sexpr
 
-import java.util.Stack
+import java.util.*
 
 fun parse(input: CharStream): SList {
-    val lexer = lexer(input)
     val exprs = Stack<SExpr>()
-    val sizes = Stack<Int>()
+    val sizes = Stack<Int>().apply { push(0) }
+    val lexer = lexer(input)
     require(lexer.nextToken() == Token.LBracket)
-    sizes.push(0)
     while (true) {
         when (val token = lexer.nextToken()) {
             Token.LBracket -> {
                 sizes.push(exprs.size)
             }
+
             Token.RBracket -> {
                 val size = exprs.size - sizes.pop()
                 val nodes = List(size) { exprs.pop() }.reversed()
@@ -20,20 +20,22 @@ fun parse(input: CharStream): SList {
                 if (sizes.isEmpty())
                     break
             }
+
             is Token.LString -> {
                 exprs.push(SAtom(token.value.toByteArray(Charsets.UTF_8)))
             }
+
             is Token.LInteger -> {
                 require(Token.Colon == lexer.nextToken())
                 val count = token.value
                 val bytes = lexer.nextBytes(count)
                 exprs.push(SAtom(bytes))
             }
+
             Token.Colon -> error("Colon outside of RLE atom.")
             Token.EOF -> error("Missing required end of list.")
         }
     }
-    require(Token.EOF == lexer.nextToken()) { "Input not completely consumed."}
-    require(1 == exprs.size)
-    return exprs.pop() as SList
+//    require(Token.EOF == lexer.nextToken()) { "Input not completely consumed." }
+    return exprs.pop().also { require(exprs.isEmpty()) } as SList
 }
